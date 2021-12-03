20b84b18722f7c5f66c318ab89e10db254d05478
Marcin Szleper <marcin.szleper@gmail.com>
2021-12-03 20:21:01 +0100

    fix non-exhaustive patterns

--- src/Day2.hs
+++ src/Day2.hs
@@ -13,7 +13,9 @@ solution = (part1, part2, "day2")
 part1 :: String -> String
 part1 = (show . processPart1) . map parseCommand . lines
   where
-    parseCommand line = case splitOn ' ' line of [pos, value] -> (pos, read value)
+    parseCommand line = case splitOn ' ' line of
+        [pos, value] -> (pos, read value)
+        _            -> ("err", -1)
 
 processPart1 :: [(String, Int)] -> Int
 processPart1 = calc 0 0
@@ -22,16 +24,20 @@ processPart1 = calc 0 0
     calc horiz depth (("forward", v) : xs) = calc (horiz + v) depth xs
     calc horiz depth (("down", v) : xs)    = calc horiz (depth + v) xs
     calc horiz depth (("up", v) : xs)      = calc horiz (depth - v) xs
+    calc horiz depth _                     = 0
 
 part2 :: String -> String
 part2 = (show . processPart2) . map parseCommand . lines
   where
-    parseCommand line = case splitOn ' ' line of [pos, value] -> (pos, read value)
+    parseCommand line = case splitOn ' ' line of
+        [pos, value] -> (pos, read value)
+        _            -> ("err", -1)
 
 processPart2 :: [(String, Int)] -> Int
 processPart2 = calc 0 0 0
   where
-    calc horiz depth aim [] = horiz * depth
+    calc horiz depth aim []                    = horiz * depth
     calc horiz depth aim (("forward", v) : xs) = calc (horiz + v) (depth + (v * aim)) aim xs
-    calc horiz depth aim (("down", v) : xs) = calc horiz depth (aim + v) xs
-    calc horiz depth aim (("up", v) : xs) = calc horiz depth (aim - v) xs
+    calc horiz depth aim (("down", v) : xs)    = calc horiz depth (aim + v) xs
+    calc horiz depth aim (("up", v) : xs)      = calc horiz depth (aim - v) xs
+    calc horiz depth aim _                     = 0
