ed9861b4647a8327c9ce62af70b7a671914f3f61
Marcin Szleper <marcin.szleper@gmail.com>
2021-12-13 20:54:14 +0100

    day 13 - simplify

--- src/Day13.hs
+++ src/Day13.hs
@@ -8,7 +8,7 @@ where
 import           Utils
 
 type Coords = (Int, Int)
-data Fold = H Int | V Int | Id deriving Show
+type Fold = [Coords] -> [Coords]
 
 type Input = ([Coords], [Fold])
 
@@ -19,57 +19,38 @@ part1 :: String -> String
 part1 = show . processPart1 . parseInput
 
 processPart1 :: Input -> Int
-processPart1 (s,f) = length $ foldl (flip toFn) s (take 1 f)
+processPart1 (s,f) = length $ foldl (\a fn -> fn a) s (take 1 f)
 
 part2 :: String -> String
 part2 = processPart2 . parseInput
 
 processPart2 :: Input -> String
-processPart2 (s,f) = unlines ("" : rows)
+processPart2 (s,f) = unlines ("" : ((\y -> (\x -> if (x,y) `elem` coords then '#' else ' ') <$> [0..maxX]) <$> [0..maxY]))
   where
-  coords = foldl (flip toFn) s f
+  coords = foldl (\a fn -> fn a) s f
   maxX = maximum $ fst <$> coords
   maxY = maximum $ snd <$> coords
-  rows = mkRow <$> [0..maxY]
-  mkRow y = (`mkChar` y) <$> [0..maxX]
-  mkChar x y = if (x,y) `elem` coords then '#' else ' '
 
 parseInput :: String -> Input
-parseInput s = (coords, folds)
+parseInput s = (parseCoord <$> c, parseFold <$> f)
   where
   [c, f] = splitOn [] $ lines s
-  coords = parseCoord <$> c
   parseCoord cc = mkCoord $ splitOn ',' cc
   mkCoord [x,y] = (read x, read y)
   mkCoord _     = undefined
-  folds = parseFold <$> f
   parseFold ff = mkFold $ splitOn '=' ff
-  mkFold ["fold along y", y] = H (read y)
-  mkFold ["fold along x", x] = V (read x)
-  mkFold _                   = Id
-
-toFn :: Fold -> [Coords] -> [Coords]
-toFn (H i) = fh i
-toFn (V i) = fv i
-toFn Id    = id
-
-fh :: Int -> [Coords] -> [Coords]
-fh i v = adjust $ nub $ translate <$> v
-  where
-  translate (x, y)
-    | y > i = (x, i - (y - i))
-    | otherwise = (x, y)
+  mkFold ["fold along y", y] = fh (read y)
+  mkFold ["fold along x", x] = fv (read x)
+  mkFold _                   = id
 
 fv :: Int -> [Coords] -> [Coords]
-fv i v = adjust $ nub $ translate <$> v
-  where
-  translate (x, y)
-    | x > i = (i - (x - i), y)
-    | otherwise = (x, y)
+fv i v = adjust $ nub $ first (\x -> if x > i then i - (x - i) else x) <$> v
+
+fh :: Int -> [Coords] -> [Coords]
+fh i v = adjust $ nub $ second (\y -> if y > i then i - (y - i) else y) <$> v
 
 adjust :: [Coords] -> [Coords]
-adjust c = adjusted
+adjust c = bimap (adjx +) (adjy +) <$> c
   where
   adjx = negate (minimum (fst <$> c))
   adjy = negate (minimum (snd <$> c))
-  adjusted = bimap (adjx +) (adjy +) <$> c
